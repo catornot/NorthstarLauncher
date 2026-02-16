934b07cd6b67be13b046372f6d7ed2bae3dfd346
catornot <41955154+catornot@users.noreply.github.com>
2026-02-15 21:39:30 -0500

    new attack vector

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "a collection of plugins for northstar related to bots";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
@@ -24,34 +24,108 @@
             config = "x86_64-w64-mingw32";
             libc = "msvcrt";
           };
+          config = {
+            allowUnsupportedSystem = true;
+            allowUnfree = true;
+            microsoftVisualStudioLicenseAccepted = true;
+          };
         };
+
+        cross = pkgs.pkgsCross.mingw-msvcrt-x86_64;
+
+        toolchainFile = sys-pkgs.writeText "WindowsToolchain.cmake" ''
+          set(CMAKE_SYSTEM_NAME Windows)
+          set(CMAKE_SYSTEM_VERSION 10.0)
+
+          set(CMAKE_GENERATE_WINDOWS_MANIFESTS OFF)
+          # set(CMAKE_CXX_COMPILER_WORKS TRUE)
+          # set(CMAKE_C_COMPILER_WORKS TRUE)
+
+          set(CMAKE_CROSSCOMPILING_EMULATOR ${sys-pkgs.wine}/bin/wine)
+        '';
       in
       {
-        nixpkgs.config.allowUnsupportedSystem = true; # hmmm doesn work?
-        formatter = sys-pkgs.nixfmt-rfc-style;
+        formatter = sys-pkgs.nixfmt-tree;
         packages = rec {
-          northstar = pkgs.callPackage ./default.nix { pkgs = pkgs; };
+          northstar =
+            with cross;
+            pkgs.windows.crossThreadsStdenv.mkDerivation {
+              pname = "NorthstarLauncher";
+              version = "0.0.0";
+              src = builtins.path { path = self; };
+
+              nativeBuildInputs = with sys-pkgs; [
+                buildPackages.cmake
+                buildPackages.ninja
+                wine
+                msitools
+                perl
+                pkgs.windows.sdk
+                pkgs.windows.mingw_w64_headers
+                pkgs.gcc
+              ];
+
+              buildPhase = ''
+                mkdir -p build
+
+                cmake -B build -G Ninja \
+                  -DCMAKE_TOOLCHAIN_FILE=${toolchainFile} \
+                  -DCMAKE_BUILD_TYPE=Release \
+                  -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+
+                cmake --build build
+              '';
+
+              installPhase = ''
+                mkdir -p $out
+                cp -r build/* $out/
+              '';
+
+              meta = {
+                description = "Northstar launcher";
+                homepage = "https://northstar.tf/";
+                license = sys-pkgs.lib.licenses.mit;
+                mainProgram = "NorthstarLauncher";
+                # platforms = [ "x86_64-w64-mingw32" "x86_64-linux" ]; # great!
+                maintainers = [ ];
+              };
+            };
           default = northstar;
         };
 
-        devShell = pkgs.mkShell rec {
-          nativeBuildInputs = with pkgs; [
-            pkgsCross.mingwW64.buildPackages.cmake
-            pkgsCross.mingwW64.buildPackages.pkg-config
-            pkgsCross.mingwW64.buildPackages.ninja
-            pkgsCross.mingwW64.buildPackages.lld
-            pkgsCross.mingwW64.buildPackages.llvm
-            pkgsCross.mingwW64.buildPackages.bintools
+        devShell = cross.mkShell rec {
+          nativeBuildInputs = [
+            cross.buildPackages.cmake
+            cross.buildPackages.pkg-config
+            cross.buildPackages.ninja
+            sys-pkgs.wine
+            sys-pkgs.perl
+
+            (sys-pkgs.writeShellApplication {
+              name = "build-ns";
+              text = ''
+                # set -e
+                # rm -rf build
+                # mkdir -p build
+
+                cmake -B build -G Ninja \
+                  -DCMAKE_BUILD_TYPE=Release \
+                  -DCMAKE_SYSTEM_NAME=Windows \
+                  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+                  -DCMAKE_REQUIRED_LIBRARIES=ws2_32 \
+                  -DCURL_USE_SCHANNEL=ON
+
+                cmake --build build
+              '';
+            })
           ];
 
-          buildInputs = with pkgs; [
-            windows.mingw_w64_headers
-            windows.mcfgthreads
-            windows.mingw_w64_pthreads
+          buildInputs = [
+            pkgs.windows.mingw_w64_headers
+            pkgs.windows.pthreads
+            pkgs.windows.sdk
+            cross.zlib
           ];
-          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
-          PATH = nixpkgs.lib.makeLibraryPath buildInputs;
-          WINEPATH = nixpkgs.lib.makeLibraryPath buildInputs;
         };
       }
     );
