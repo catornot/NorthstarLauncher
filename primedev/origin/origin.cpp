691f7eeb4b06ceb2b17fc9c267aa43880b08ad4b
catornot <41955154+catornot@users.noreply.github.com>
2026-04-13 17:53:03 -0400

    fmt

--- primedev/origin/origin.cpp
+++ primedev/origin/origin.cpp
@@ -7,24 +7,29 @@ static bool __fastcall h_CheckIfOriginIsInstalled()
 	if (!strstr(GetCommandLineA(), "-noOriginStartup"))
 		return o_pCheckIfOriginIsInstalled();
 
-    if (o_pCheckIfOriginIsInstalled()) {
-        NS::log::NORTHSTAR->warn("Origin is NOT installed according to OriginSDK's check (HKLM\\SOFTWARE\\Wow6432Node\\Origin\\ClientPath is missing/empty).");
-        NS::log::NORTHSTAR->warn("We are bypassing this check in case LSX server is remotely ran (such as in Linux in another WINE prefix), but note that things will fail if Origin is actually not running.");
-    }
+	if (o_pCheckIfOriginIsInstalled())
+	{
+		NS::log::NORTHSTAR->warn(
+			"Origin is NOT installed according to OriginSDK's check (HKLM\\SOFTWARE\\Wow6432Node\\Origin\\ClientPath is missing/empty).");
+		NS::log::NORTHSTAR->warn("We are bypassing this check in case LSX server is remotely ran (such as in Linux in another WINE "
+								 "prefix), but note that things will fail if Origin is actually not running.");
+	}
 
-    return true;
+	return true;
 }
 
-static uint64_t(__fastcall* o_pTryToStartOrigin)(void * a1) = nullptr;
-static uint64_t __fastcall h_TryToStartOrigin(void * a1)
+static uint64_t(__fastcall* o_pTryToStartOrigin)(void* a1) = nullptr;
+static uint64_t __fastcall h_TryToStartOrigin(void* a1)
 {
 	if (!strstr(GetCommandLineA(), "-noOriginStartup"))
 		return o_pTryToStartOrigin(a1);
 
-	if (o_pTryToStartOrigin(a1) != 0) {
-        NS::log::NORTHSTAR->warn("Origin process has failed to start. We are ignoring this and let it fail on LSX connection attempt, because LSX might still be up regardless, even if this failed.");
+	if (o_pTryToStartOrigin(a1) != 0)
+	{
+		NS::log::NORTHSTAR->warn("Origin process has failed to start. We are ignoring this and let it fail on LSX connection attempt, "
+								 "because LSX might still be up regardless, even if this failed.");
 	}
-    return 0;
+	return 0;
 }
 
 ON_DLL_LOAD("OriginSDK.dll", OriginSDKFuncs, (CModule module))
