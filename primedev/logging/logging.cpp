b01f2e309f19adb294cf884c322bc3048db7630f
catornot <41955154+catornot@users.noreply.github.com>
2026-04-13 02:37:52 -0400

    add a noallocconsole option

--- primedev/logging/logging.cpp
+++ primedev/logging/logging.cpp
@@ -122,8 +122,10 @@ void CustomSink::custom_log(const custom_log_msg& msg)
 
 void InitialiseConsole()
 {
-	if (AllocConsole() != FALSE)
-	{
+	if (strstr(GetCommandLineA(), "-noallocconsole")) {
+		FreeConsole();
+		return;
+	} else if (AllocConsole() != FALSE)	{
 		freopen("CONOUT$", "w", stdout);
 		freopen("CONOUT$", "w", stderr);
 	}
