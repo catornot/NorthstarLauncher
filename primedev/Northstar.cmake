d972b50c338758c1bbaf132ebee2bf33c92d125b
catornot <41955154+catornot@users.noreply.github.com>
2026-08-06 00:43:23 -0400

    remove empty header

--- primedev/Northstar.cmake
+++ primedev/Northstar.cmake
@@ -139,7 +139,6 @@ add_library(
     "server/servernethooks.cpp"
     "server/serverpresence.cpp"
     "server/serverpresence.h"
-    "server/idlekick.h"
     "server/idlekick.cpp"
     "shared/exploit_fixes/exploitfixes.cpp"
     "shared/exploit_fixes/exploitfixes_lzss.cpp"
